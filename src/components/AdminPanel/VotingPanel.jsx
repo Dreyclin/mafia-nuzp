ae2b17a0f81a04d00b7edcdb44890e1640e66492
Dreyclin <makarka16@gmail.com>
2024-05-10 18:56:41 +0300

    Add: voting panel layout

--- src/components/AdminPanel/VotingPanel.jsx
+++ src/components/AdminPanel/VotingPanel.jsx
@@ -3,7 +3,33 @@ import React from "react";
 export default function VotingPanel() {
     return (
         <div className="voting-panel">
-
+            <h2>Голосование: </h2>
+            <h3>Кандидаты:</h3>
+            <div className="candidates-container">
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+                <div className="candidate"></div>
+            </div>
+            <h3>Голоса:</h3>
+            <div className="votes-container">
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+                <div className="vote"></div>
+            </div>
         </div>
     )
 }
