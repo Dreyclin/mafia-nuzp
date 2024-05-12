e67c390096083739b4eddfa37add4df5b6ea0f1e
Maxim Sur <makarka16@gmail.com>
2024-05-13 00:47:17 +0300

    Delete: increase time thunk

--- src/features/game/timerSlice.js
+++ src/features/game/timerSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
@@ -10,36 +10,34 @@ const initialState = {
 export const loadTimer = createAsyncThunk(
     'timer/load',
     async (data) => {
-        const response = await axios.post("http://localhost:5000/loadTimer", {data})
-        return response.data;
+        try {
+            const response = await axios.post("http://localhost:5000/loadTimer", { data })
+            return response.data;
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 )
 
-export const increaseTime = createAsyncThunk(
-    'timer/increase',
-    async (data) => {
-        const response = await axios.post("http://localhost:5000/increase", {data})
-        return response.data
-    }
-)
 
 const timerSlice = createSlice({
     name: "timer",
     initialState,
     extraReducers: (builder) => {
         builder
-        
-        .addCase(loadTimer.fulfilled, (state, action) => {
-            state.time = action.payload
-        })
 
-        .addCase(increaseTime.fulfilled, (state, action) => {
-            let {minutes, seconds} = state.time;
-            let {newMin, newSec} = action.payload;
+            .addCase(loadTimer.fulfilled, (state, action) => {
+                state.time = action.payload
+            })
 
-            minutes = newMin;
-            seconds = newSec;
-        })
+            .addCase(controlAmountTime.fulfilled, (state, action) => {
+                let { minutes, seconds } = state.time;
+                let { newMin, newSec } = action.payload;
+
+                minutes = newMin;
+                seconds = newSec;
+            })
     }
 })
 
