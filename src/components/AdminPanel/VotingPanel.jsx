b495179aee298732fa02927f8260c312ac63667e
Dreyclin <makarka16@gmail.com>
2024-05-16 17:35:11 +0300

    Add: votes field to players

--- src/components/AdminPanel/VotingPanel.jsx
+++ src/components/AdminPanel/VotingPanel.jsx
@@ -13,6 +13,16 @@ export default function VotingPanel(props) {
                             <span>{candidate.number} {candidate.votes != null && `(${candidate.votes})`} </span>
                         </div>
                         <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>10</button>
+
                     </div>
                 })}
             </div>
