424bf0bf5d4df1778f247e90878c492b2921a1a9
Maxim Sur <makarka16@gmail.com>
2024-05-17 00:57:32 +0300

    Add: voting logic

--- src/components/AdminPanel/VotingPanel.jsx
+++ src/components/AdminPanel/VotingPanel.jsx
@@ -2,45 +2,58 @@ import React from "react";
 import { useDispatch } from "react-redux";
 
 export default function VotingPanel(props) {
-
     const dispatch = useDispatch();
+
     return (
         <div className="voting-panel">
             <h2>Голосование: </h2>
             <h3>Кандидаты:</h3>
             <div className="candidates-container">
-                {props.candidates != null && props.candidates.map(candidate => {
-                    // number: 2, votes: null
-                    // playerInGameLocal = 10;
-                    let playersInGameLocal = props.playersInGame.length;
-                    let numOfButtons = playersInGameLocal - candidate.votes;
-                    // candidate[0].votes = 5;
-                    //candidatate[1].votes = null;
-                    return (
-                        <div className="candidate">
-                            <div>
-                                <span>{candidate.number} {candidate.votes != null && `(${candidate.votes})`} </span>
+                {props.candidates != null && props.candidates.map((candidate, index) => {
+                    if (candidate.isVoted) {
+                        return (
+                            <div className="candidate" key={candidate.number}>
+                                <div>
+                                    <span>{candidate.number} {candidate.votes != null && `(${candidate.votes})`}</span>
+                                </div>
                             </div>
-                            {}
-                            {props.playersInGame.map(player => {
-                                let number = 0;
-                                if (candidate.votes != null) {
-                                    if (!candidate.isVoted) {
-                                        return <button
-                                            id={candidate.number}
-                                            onClick={(event) => dispatch(props.voteForPlayer({ candidate: event.target.id, votes: event.target.textContent }))}>{number}</button>
-                                    }
-                                } else {
-                                    return <button
-                                        id={candidate.number}
-                                        onClick={(event) => dispatch(props.voteForPlayer({ candidate: event.target.id, votes: event.target.textContent }))}>{number}</button>
+                        )
+                    }
+
+                    const remainingVotes = props.playersInGame.length - props.candidates.reduce((acc, candidate) => acc + (candidate.votes || 0), 0);
+                    const numOfButtons = remainingVotes;
+
+                    if (index === props.candidates.length - 1) {
+                        if(props.candidates.length > 1){
+                            const unVoted = props.candidates.filter(candidate => candidate.isVoted === false)
+                            if(unVoted.length === 1){
+                                if (remainingVotes > 0 && !unVoted[0].votes) {
+                                    dispatch(props.voteForPlayer({ candidate: unVoted[0].number, votes: remainingVotes }));
                                 }
-                                number++;
+                            }
+                        }
+                    }
+
+                    return (
+                        <div className="candidate" key={candidate.number}>
+                            <div>
+                                <span>{candidate.number} {candidate.votes != null && `(${candidate.votes})`}</span>
+                            </div>
+                            {Array.from({ length: numOfButtons }, (_, index) => {
+                                let number = index + 1;
+                                return (
+                                    <button
+                                        key={`${candidate.number}-${number}`}
+                                        id={`${candidate.number}-${number}`}
+                                        onClick={(event) => dispatch(props.voteForPlayer({ candidate: candidate.number, votes: number }))}>
+                                        {number}
+                                    </button>
+                                );
                             })}
                         </div>
-                    )
+                    );
                 })}
             </div>
         </div>
-    )
+    );
 }
